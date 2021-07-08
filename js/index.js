ae613b840ef0a544409ec06dcdfd3045ae6888dd
Calvin Chan <architectcchan@gmail.com>
2021-07-07 19:42:49 -0700

    added js file

---
+++ js/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+
+let distance = $('#distance').val();
+
+let time = $('#time').val();
+let pace = distance/time;
+
+let submit = $('#submit');
+// submit.click(() => {
+    
+// });
+
+// Once form submitted, two new circles of analytics will show up above the
+// current analytics: 1. Calories burned today 2. Pace Today.
+
+//add to a data table of data. 
+//can view average pace this week. Amount of increase in speed.
+// Amount of hours of running this week.
